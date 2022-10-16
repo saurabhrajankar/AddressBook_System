ae2ade9619d5f5b6c0bfb4d16d3abe8f4e3b1730
saurabhrajankar <112363658+saurabhrajankar@users.noreply.github.com>
2022-10-16 15:39:43 +0530

    Add files via upload

---
+++ UC1-Create_Contact/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UC1_Create_Contact
+{
+    class Create_Contact
+    {
+        class Contact
+        {
+            public string firstName { get; set; }
+            public string lastName { get; set; }
+            public string address { get; set; }
+            public string state { get; set; }
+            public long phoneNumber { get; set; }
+            public string email { get; set; }
+            public int zipCode { get; set; }
+        }
+        List<Contact> People = new List<Contact>();
+        public void GetContactDetails()   // creating contact details of person
+        {
+            Contact contact = new Contact();
+            Console.WriteLine("Enter the First Name");
+            contact.firstName = Console.ReadLine();
+            Console.WriteLine("Enter the Last Name");
+            contact.lastName = Console.ReadLine();
+            Console.WriteLine("Enter the Adresss");
+            contact.address = Console.ReadLine();
+            Console.WriteLine("Enter the State");
+            contact.state = Console.ReadLine();
+            Console.WriteLine("Enter the Zip Code");
+            contact.zipCode = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Phone Number");
+            contact.phoneNumber = Convert.ToInt64(Console.ReadLine());
+            Console.WriteLine("Enter the Email");
+            contact.email = Console.ReadLine();
+            People.Add(contact);
+        }
+        public static void Main(String[] args)
+        {
+            Create_Contact AddressBook = new Create_Contact();
+            AddressBook.GetContactDetails();
+            Console.ReadLine();
+        }
+    }
+}
